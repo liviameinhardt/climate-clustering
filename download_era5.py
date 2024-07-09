b9ee6660c4b439153c6e9466bb154e8a693fbd42
Livia Meinhardt <livia.meinhardt@HPCGPU.strlx11j5mwe1o0eps222jojyb.xx.internal.cloudapp.net>
2024-07-09 11:53:40 -0300

    test concluded

--- download_era5.py
+++ download_era5.py
@@ -20,87 +20,70 @@ Notes:
 !pip install h5netcdf
 
 """
-import sys
+#%%
 import os
 import cdsapi
 import cfgrib
 import xarray as xr
-
+import numpy as np
 from time import sleep
 
 API_KEY =  "202370:f787b403-6f87-40ee-b128-22625fee3ec7"
 cds = cdsapi.Client(url="https://cds.climate.copernicus.eu/api/v2",key=API_KEY)
-area_download = [2.46, -71.97, -33.26,-36.64] #brazil area
 
-variables = [
-    '2m_temperature',
-    'total_precipitation',
-    'mean_sea_level_pressure'
-]
+def download(area_download,years_to_download,times_to_get,variables):
 
-years_to_download = [str(i) for i in range(1995,2000)]
+    #download blocks
+    block_size = 6
+    block_start, block_end = 0, block_size
 
-times_available = [ '00:00', '01:00', '02:00',
-                  '03:00', '04:00', '05:00',
-                  '06:00', '07:00', '08:00',
-                  '09:00', '10:00', '11:00',
-                  '12:00', '13:00', '14:00',
-                  '15:00', '16:00', '17:00',
-                  '18:00', '19:00', '20:00',
-                  '21:00', '22:00', '23:00',]
+    datasets = []
+    while block_start < len(years_to_download):
 
-times_to_get = times_available[::3]
+        cur_years = years_to_download[block_start:block_end]
+        start_y, end_y = cur_years[0],cur_years[-1]
 
-#download blocks
-block_size = 6
-block_start, block_end = 0, block_size
+        print('Downloading',cur_years)
 
-datasets = []
-while block_start < len(years_to_download):
+        dataset_name =f"era_{start_y}_{end_y}"
+        dataset_name =f'data/raw/{dataset_name}.grib'
 
-    cur_years = years_to_download[block_start:block_end]
-    start_y, end_y = cur_years[0],cur_years[-1]
+        datasets.append(dataset_name)
+        
+        data = cds.retrieve(
+            'reanalysis-era5-single-levels',
+            {
+                'product_type': 'reanalysis',
+                'variable': variables,
 
-    print('Downloading',cur_years)
+                'year': cur_years,
 
-    dataset_name =f"era_{start_y}_{end_y}"
-    dataset_name =f'data/raw/{dataset_name}.grib'
+                'month': [
+                    '01', '02', '03','04', '05', '06',
+                    '07', '08', '09','10', '11', '12'],
 
-    datasets.append(dataset_name)
-    
-    data = cds.retrieve(
-        'reanalysis-era5-single-levels',
-        {
-            'product_type': 'reanalysis',
-            'variable': variables,
+                'day': [
+                    '01', '02', '03','04', '05', '06',
+                    '07', '08', '09','10', '11', '12',
+                    '13', '14', '15','16', '17', '18',
+                    '19', '20', '21','22', '23', '24',
+                    '25', '26', '27','28', '29', '30','31'],
 
-            'year': cur_years,
+                'time': times_to_get,
 
-            'month': [
-                '01', '02', '03','04', '05', '06',
-                '07', '08', '09','10', '11', '12'],
+                'area': [2.46, -71.97, -33.26,-36.64], #Brazil box http://bboxfinder.com/#-33.268014,-71.975849,2.469466,-36.648545
 
-            'day': [
-                '01', '02', '03','04', '05', '06',
-                '07', '08', '09','10', '11', '12',
-                '13', '14', '15','16', '17', '18',
-                '19', '20', '21','22', '23', '24',
-                '25', '26', '27','28', '29', '30','31'],
+                'format': 'grib',
 
-            'time': times_to_get,
+            },dataset_name)
 
-            'area': [2.46, -71.97, -33.26,-36.64], #Brazil box http://bboxfinder.com/#-33.268014,-71.975849,2.469466,-36.648545
+        block_start = block_end
+        block_end += block_size
 
-            'format': 'grib',
+        print('-- finished --')
 
-        },dataset_name)
+        sleep(10)
 
-    block_start = block_end
-    block_end += block_size
-
-    print('-- finished --')
-
-    sleep(10)
 
 def concat_files():
     
@@ -122,3 +105,51 @@ def concat_files():
     ds = xr.concat(ds_list, dim='time')
     output_file = 'data/msl_t2m_1983_2023.nc'
     ds.to_netcdf(output_file)
+
+
+def save_values(file):
+    #just to save process
+
+    data = cfgrib.open_dataset('data/raw/' + file)
+    df = data.t2m.values
+
+    q99 = np.quantile(df,0.99)
+    q10 = np.quantile(df,0.1)
+
+    norm_imgs = np.clip(df, q10, q99)
+    norm_imgs = (norm_imgs - q10)/(q99 -q10)
+
+    time, lat, long = norm_imgs.shape
+
+    wind = 8 #find best after
+
+    norm_imgs = norm_imgs.reshape(int(time/wind), wind, lat, long)
+    norm_imgs = np.transpose(norm_imgs, (0, 2, 3, 1))
+
+    np.save('data/test',norm_imgs,allow_pickle=False)
+
+
+###################
+
+area_download = [2.46, -71.97, -33.26,-36.64] #brazil area
+
+variables = [
+    '2m_temperature',
+    'total_precipitation',
+    'mean_sea_level_pressure'
+]
+
+years_to_download = [str(i) for i in range(1995,2000)]
+
+times_available = [ '00:00', '01:00', '02:00',
+                  '03:00', '04:00', '05:00',
+                  '06:00', '07:00', '08:00',
+                  '09:00', '10:00', '11:00',
+                  '12:00', '13:00', '14:00',
+                  '15:00', '16:00', '17:00',
+                  '18:00', '19:00', '20:00',
+                  '21:00', '22:00', '23:00',]
+
+times_to_get = times_available[::3]
+
+# download(area_download,years_to_download,times_to_get,variables)
